60321d9673f24bb1896023b9e9df20aed141136a
Dmitry Dubovitsky <luxoft.dubovitskii@extaccount.com>
2021-07-02 17:00:13 +0300

    Upd out ex

--- out1/vm-plus-ssh.tf
+++ out1/vm-plus-ssh.tf
@@ -27,6 +27,14 @@ output "VM-name" {
   value = aws_instance.web.tags.Name
 }
 
+output "VM-root-device" {
+  value = aws_instance.web.root_block_device
+}
+
 output "ip-address" {
   value = aws_instance.web.private_ip
+}
+
+output "ssh-public-key" {
+  value = tls_private_key.tf-training.public_key_openssh
 }
