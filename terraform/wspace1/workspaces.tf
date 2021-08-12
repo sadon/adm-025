dce31e6a527a6b39ff9cf222a483cdd9f6d168ea
Dmitry Dubovitsky <luxoft.dubovitskii@extaccount.com>
2021-08-12 11:35:29 +0300

    misc

--- terraform/wspace1/workspaces.tf
+++ terraform/wspace1/workspaces.tf
@@ -1,4 +1,11 @@
 
-resource "null_resource" "test" {
+resource "random_string" "suffix" {
+  length = 10
+  special = false
+  upper = false
+}
 
+
+resource "aws_s3_bucket" "images" {
+  bucket = "luxoft-adm025-wspace-${terraform.workspace}-${random_string.suffix.result}"
 }
